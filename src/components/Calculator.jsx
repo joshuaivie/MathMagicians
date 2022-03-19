428d56c932dd5b45c4aaa65bf0beddce6f933c54
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-03-19 17:55:54 +0100

    Add Calculator Input Handlers

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,31 +1,193 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 import './Calculator.scss';
+import calculate from '../logic/calculate';
 
 export default class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      calculatorObject: {},
+      display: '',
+    };
+    this.handleCalulatorInput = this.handleCalulatorInput.bind(this);
+    this.updateDisplay = this.updateDisplay.bind(this);
+  }
+
+  handleCalulatorInput = (event) => {
+    const input = event.target.innerText;
+    this.setState(
+      (previousState) => ({
+        ...previousState,
+        calculatorObject: calculate(previousState.calculatorObject, input),
+      }),
+      () => {
+        this.updateDisplay();
+      },
+    );
+  };
+
+  updateDisplay = () => {
+    this.setState((previousState) => ({
+      ...previousState,
+      display: `${previousState.calculatorObject?.total || ''} ${previousState.calculatorObject?.operation || ''} ${previousState.calculatorObject?.next || ''}`.trim(),
+    }));
+  };
+
   render() {
+    const { display } = this.state;
     return (
       <div className="calculator">
-        <input className="display span-4" id="display" type="text" />
-        <button className="button grey" id="clear-screen" type="button">AC</button>
-        <button className="button grey" id="polarity-operator" type="button">+/-</button>
-        <button className="button grey" id="modulus-operator" type="button">%</button>
-        <button className="button orange" id="division-operator" type="button">÷</button>
-        <button className="button grey" id="number-7" type="button">7</button>
-        <button className="button grey" id="number-8" type="button">8</button>
-        <button className="button grey" id="number-9" type="button">9</button>
-        <button className="button orange" id="multiplication-operator" type="button">x</button>
-        <button className="button grey" id="number-4" type="button">4</button>
-        <button className="button grey" id="number-5" type="button">5</button>
-        <button className="button grey" id="number-6" type="button">6</button>
-        <button className="button orange" id="suubtraction-operator" type="button">-</button>
-        <button className="button grey" id="number-1" type="button">1</button>
-        <button className="button grey" id="number-2" type="button">2</button>
-        <button className="button grey" id="number-3" type="button">3</button>
-        <button className="button orange" id="addition-operator" type="button">+</button>
-        <button className="button grey span-2" id="number-0" type="button">0</button>
-        <button className="button grey" id="decimal" type="button">.</button>
-        <button className="button orange" id="equality-operator" type="button">=</button>
+        <div
+          className="display span-4"
+          id="display"
+          type="text"
+          disabled
+        >
+          {display || '0'}
+        </div>
+        <button
+          onClick={this.handleCalulatorInput}
+          className="button grey"
+          type="button"
+        >
+          AC
+        </button>
+        <button
+          onClick={this.handleCalulatorInput}
+          className="button grey"
+          id="polarity-operator"
+          type="button"
+        >
+          +/-
+        </button>
+        <button
+          onClick={this.handleCalulatorInput}
+          className="button grey"
+          id="modulus-operator"
+          type="button"
+        >
+          %
+        </button>
+        <button
+          onClick={this.handleCalulatorInput}
+          className="button orange"
+          id="division-operator"
+          type="button"
+        >
+          ÷
+        </button>
+        <button
+          onClick={this.handleCalulatorInput}
+          className="button grey"
+          type="button"
+        >
+          7
+        </button>
+        <button
+          onClick={this.handleCalulatorInput}
+          className="button grey"
+          type="button"
+        >
+          8
+        </button>
+        <button
+          onClick={this.handleCalulatorInput}
+          className="button grey"
+          type="button"
+        >
+          9
+        </button>
+        <button
+          onClick={this.handleCalulatorInput}
+          className="button orange"
+          id="multiplication-operator"
+          type="button"
+        >
+          x
+        </button>
+        <button
+          onClick={this.handleCalulatorInput}
+          className="button grey"
+          type="button"
+        >
+          4
+        </button>
+        <button
+          onClick={this.handleCalulatorInput}
+          className="button grey"
+          type="button"
+        >
+          5
+        </button>
+        <button
+          onClick={this.handleCalulatorInput}
+          className="button grey"
+          type="button"
+        >
+          6
+        </button>
+        <button
+          onClick={this.handleCalulatorInput}
+          className="button orange"
+          id="suubtraction-operator"
+          type="button"
+        >
+          -
+        </button>
+        <button
+          onClick={this.handleCalulatorInput}
+          className="button grey"
+          type="button"
+        >
+          1
+        </button>
+        <button
+          onClick={this.handleCalulatorInput}
+          className="button grey"
+          type="button"
+        >
+          2
+        </button>
+        <button
+          onClick={this.handleCalulatorInput}
+          className="button grey"
+          type="button"
+        >
+          3
+        </button>
+        <button
+          onClick={this.handleCalulatorInput}
+          className="button orange"
+          id="addition-operator"
+          type="button"
+        >
+          +
+        </button>
+        <button
+          onClick={this.handleCalulatorInput}
+          className="button grey span-2"
+          id="number-0"
+          type="button"
+        >
+          0
+        </button>
+        <button
+          onClick={this.handleCalulatorInput}
+          className="button grey"
+          id="decimal"
+          type="button"
+        >
+          .
+        </button>
+        <button
+          onClick={this.handleCalulatorInput}
+          className="button orange"
+          id="equality-operator"
+          type="button"
+        >
+          =
+        </button>
       </div>
     );
   }
